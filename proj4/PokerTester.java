b92d2fa9c9eaa6e03b3689a94dca7d17ce7cb3b5
Chris Manna <nitro7777@users.noreply.github.com>
2015-11-12 23:50:08 -0500

    Update PokerTester.java

--- proj4/PokerTester.java
+++ proj4/PokerTester.java
@@ -1,1 +1,22 @@
-Inof Here
+import java.util.Scanner;
+
+public class PokerTester {
+
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Poker!\n");
+		Scanner in = new Scanner(System.in);
+		boolean playAgain = true;
+		while(playAgain) {
+			Game game = new Game();
+			game.play();
+			System.out.println("Would you like to play again? press y to continue, q to quit");
+			if(in.next().equals("q"))
+				playAgain = false;
+		}
+		System.out.println("Thank you for playing");
+
+	}
+
+}
