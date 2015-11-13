675abf5831421382fe7cca0a31358441fd550a0c
Chris Manna <nitro7777@users.noreply.github.com>
2015-11-12 23:44:11 -0500

    Update SodaCan.java

--- proj2/SodaCan.java
+++ proj2/SodaCan.java
@@ -1,1 +1,24 @@
-info here
+public class SodaCan
+{ 
+	//class SodaCan constructor can access these variables. 
+	//The user cannot.
+	private double canHeight;
+	private double canRadius;
+	
+	//other classes can use this to change the variables 
+	//into constructor variables. 
+	public SodaCan(double height , double diameter)
+	{
+		canHeight = height;
+		canRadius = diameter/2;
+	}
+	public double getVolume()
+	{
+		return Math.PI*canRadius*canRadius*canHeight;
+	}
+	
+	public double getSurfaceArea()
+	{
+		return Math.PI*canRadius*2*canRadius+Math.PI*canRadius*2*canHeight;
+	}
+}
