6e29bc7831645d13eea12d5d2e288298366d1de5
Chris Manna <nitro7777@users.noreply.github.com>
2015-11-12 23:43:42 -0500

    Update RootTester.java

--- proj2/RootTester.java
+++ proj2/RootTester.java
@@ -1,1 +1,29 @@
-info here
+/*Welcome to my RootTester program where we use the Heron Method to
+ * compute an approximate root value for the values the user suggests. 
+ * From Heron out you'll be reading my code, (Bada chshh).
+ */
+
+import java.util.Scanner;
+
+public class RootTester
+{
+	public static void main(String[] args)
+	{
+		double number;
+		final double EPSILON = .005;
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Please put the value you'd like to ");
+		System.out.println("approximate the root for: ");
+		number = input.nextDouble();
+		
+		RootMap approxValue = new RootMap(number, EPSILON); 
+		// approxValue is now object of RootMap with parameter number.
+		// we pass the variable number to RootMap
+			
+		
+		while (approxValue.hasMoreGuesses())
+			System.out.println(approxValue.nextGuess());
+		
+	}
+}
