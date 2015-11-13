76874c55a238dd2a7e52d057207f69f1c0423420
Chris Manna <nitro7777@users.noreply.github.com>
2015-11-12 23:53:10 -0500

    Update Main.java

--- proj5/Main.java
+++ proj5/Main.java
@@ -1,1 +1,32 @@
-Info Here
+import java.util.Scanner;
+import java.io.*;
+/**
+ * Write a description of class Main here.
+ * 
+ * @author (Christopher G Manna) 
+ * @version (11/21/11)
+ */
+public class Main
+{
+    // instance variables - replace the example below with your own
+    private String keyWord;
+    private Scanner userInput;
+    private String alphRev;
+    private String newAlph;
+    private String adjustedAlphRev;
+  public static void main(String[] args) throws IOException
+  {
+    File inFile = new File(args[0]);
+    Scanner input = new Scanner(inFile);
+    Encrypt() e = new Encrypt();
+    input.crypting();
+    
+    PrintWriter output = new PrintWriter(args[1]);
+    while(input.hasNext())
+    {
+      keyWord=input.next();
+      output.print(keyWord);
+    }
+      output.close();
+  }
+}
