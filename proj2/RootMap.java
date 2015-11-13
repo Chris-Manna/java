49de1164f0616f834999a86148d290adb09b0bbe
Chris Manna <nitro7777@users.noreply.github.com>
2015-11-12 23:42:49 -0500

    works

--- proj2/RootMap.java
+++ proj2/RootMap.java
@@ -1,1 +1,44 @@
-info here
+public class RootMap
+{//values that exist within RootMap
+	public boolean valid;
+	private double initGuess;
+	private double betterGuess;
+	private double numberToTest;
+	private double difference;
+	private double actualDifference;
+
+	public RootMap(double number, final double EPSILON)
+	{
+	numberToTest = number;
+	difference = EPSILON;
+	initGuess = 1;
+	betterGuess = (initGuess + numberToTest/initGuess)/2;
+	valid = true;
+	}
+	
+	private void calcNextGuess()
+	{
+	//exists within nextGuess and cannot be seen or accessed by user
+		initGuess = betterGuess;
+		betterGuess = (initGuess + numberToTest/initGuess)/2;
+	}
+	public double nextGuess()
+	{
+	//is summoned by the user.
+		calcNextGuess();
+		return betterGuess;
+	}
+	private void calcDifference()
+	{//exists within hasMoreGuesses method and cannot be seen or accessed by user
+		actualDifference = initGuess - betterGuess;
+		if (actualDifference<0)
+				actualDifference = actualDifference*(-1);
+	}
+	public boolean hasMoreGuesses()
+	{//can be accessed by the user
+		calcDifference();
+		if (actualDifference <= difference)
+			valid = false;
+		return valid;
+	}
+}
